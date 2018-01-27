fbd0cc1418feacd9de65a7163b3b6617c78a1ecc
Jae Henderson <jae.henderson@Jaes-MacBook-Pro-15-SN-C02S71YPG8WM.local>
2018-01-26 19:03:22 -0800

    trying new way to add kje files

---
+++ KJECalc/CompareLoan3Params.js
@@ -0,0 +1,34 @@
+
+
+
+KJE.parameters.set("COMPARE_TYPE","LOAN");
+KJE.parameters.set("INTEREST_RATE1",3.50);
+KJE.parameters.set("INTEREST_RATE2",4.00);
+KJE.parameters.set("INTEREST_RATE3",4.50);
+KJE.parameters.set("LOAN_AMOUNT",200000);
+KJE.parameters.set("LOAN_TERM1",15);
+KJE.parameters.set("LOAN_TERM2",20);
+KJE.parameters.set("LOAN_TERM3",30);
+KJE.parameters.set("MSG_MONTHLY_PAYMENT1","Monthly payment");
+KJE.parameters.set("MSG_MONTHLY_PAYMENT2","Monthly payment");
+KJE.parameters.set("MSG_MONTHLY_PAYMENT3","Monthly payment");
+KJE.parameters.set("MSG_INTEREST_RATE1","15 year");
+KJE.parameters.set("MSG_INTEREST_RATE2","20 year");
+KJE.parameters.set("MSG_INTEREST_RATE3","30 year");
+
+KJE.parameters.set("PAYMENT_TYPE1",KJE.Default.PAY_MONTHLY);
+KJE.parameters.set("PAYMENT_TYPE2",KJE.Default.PAY_MONTHLY);
+KJE.parameters.set("PAYMENT_TYPE3",KJE.Default.PAY_MONTHLY);
+KJE.CalculatorWidthMax = 250;
+
+
+
+/**V3_CUSTOM_CODE**/
+/* <!--
+  Financial Calculators, &copy;1998-2017 KJE Computer Solutions, Inc.
+  For more information please see:
+  <A HREF="http://www.dinkytown.net">http://www.dinkytown.net</A>
+ -->
+ */
+if (KJE.IE7and8) KJE.init();
+
